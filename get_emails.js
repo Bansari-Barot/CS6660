dd9e8f37152fe589cf90dcd4be89c43ccd243ad7
bbthummar <38369955+bbthummar@users.noreply.github.com>
2018-05-30 17:36:59 -0700

    Add files via upload

---
+++ get_emails.js
@@ -0,0 +1,11 @@
+// A script to read from a collection of stores in the local database
+conn = new Mongo();
+
+// connect to the local database
+db = conn.getDB("local");
+
+// Queries
+result = db.slack.find({},{_id:0,email:1})
+while ( result.hasNext() ) {
+   printjson( result.next());
+}
